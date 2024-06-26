fe21198ce7fde09bfa6e9e80056dcabcd5a70372
kushal <kuchibhotlakushal2004@gmail.com>
2024-06-26 17:18:44 +0530

    modified:   src/components/ui/GitHubCalendarComponent.tsx

--- src/components/ui/GitHubCalendarComponent.tsx
+++ src/components/ui/GitHubCalendarComponent.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import GitHubCalendar from 'react-github-calendar';
+import ActivityCalendar from 'react-activity-calendar';
 
 interface GitHubCalendarComponentProps {
   username: string;
 }
 
+// Example hardcoded data
+const data = [
+  {
+    date: '2023-06-14',
+    count: 2,
+    level: 1
+  },
+  {
+    date: '2023-06-22',
+    count: 16,
+    level: 3
+  }
+];
+
 const GitHubCalendarComponent: React.FC<GitHubCalendarComponentProps> = ({ username }) => {
-  return <GitHubCalendar username={username} />;
+  return (
+    <ActivityCalendar
+      data={data}
+      theme={{
+        light: ['#f0f0f0', '#c4edde', '#7ac7c4', '#f73859', '#384259'],
+        dark: ['#383838', '#4D455D', '#7DB9B6', '#F5E9CF', '#E96479'],
+      }}
+      blockMargin={4}
+      blockSize={12}
+      hideColorLegend={false}
+      labels={{
+        legend: {
+          less: 'Less',
+          more: 'More',
+        },
+      }}
+    />
+  );
 };
 
 export default GitHubCalendarComponent;
